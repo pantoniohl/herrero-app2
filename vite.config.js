fac39d15315b30921cc773a7f03c31983e082149
pantoniohl <pantoniohl@gmail.com>
2026-03-09 01:44:27 +0100

    force new bundle name v65

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/app-[hash].js',
+        chunkFileNames: 'assets/chunk-[hash].js',
+      }
+    }
+  }
 })
-// utf8 fix 
-
-// force rebuild v62 1773015658647
+// v65 1773017067710
